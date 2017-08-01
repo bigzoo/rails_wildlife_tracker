97cf771a111c0d022d7918c29f9af38fb6e5d0cc
Chris Nyaga <nyagachris75@gmail.com>
2017-08-01 15:43:22 +0300

    Sightings controller created and added CRUD funtionality for performing various methods on sighing objects.

---
+++ app/controllers/sightings_controller.rb
@@ -0,0 +1,40 @@
+class SightingsController < ApplicationController
+  def new
+    @animal = Animal.find(params[:animal_id])
+    @sighting = Sighting.new
+  end
+  def create
+    @animal = Animal.find(params[:animal_id])
+    @sighting = @animal.sightings.new(sighting_params)
+    if @sighting.save
+      redirect_to animal_path(@animal)
+    else
+      render :new
+    end
+  end
+  def edit
+    @animal = Animal.find(params[:animal_id])
+    @sighting = Sighting.find(params[:id])
+  end
+  def update
+    @animal = Animal.find(params[:animal_id])
+    @sighting = Sighting.find(params[:id])
+    if @sighting.update(sighting_params)
+      redirect_to animal_path(@animal)
+    else
+      render :edit
+    end
+  end
+  def destroy
+    @animal = Animal.find(params[:animal_id])
+    @sighting = Sighting.find(params[:id])
+    @sighting.destroy
+    redirect_to animal_path(@animal)
+  end
+
+
+  private
+  def sighting_params
+    params.require(:sighting).permit(:date,:longitude,:latitude)
+  end
+end
