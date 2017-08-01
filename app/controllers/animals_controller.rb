cccaec9fd622b82654ce56f24c6b67ca10b835d3
Chris Nyaga <nyagachris75@gmail.com>
2017-08-01 15:39:45 +0300

    Animals controller updated with CRUD funtionality for various routes.

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -1,2 +1,43 @@
 class AnimalsController < ApplicationController
+  def index
+    @animals = Animal.all
+  end
+  def new
+    @animal = Animal.new
+  end
+  def create
+    @animal = Animal.new(animal_params)
+    if @animal.save
+      flash[:notice] = "Animal successfully added!"
+      redirect_to  animals_path
+    else
+      render :new
+    end
+  end
+  def show
+    @animal = Animal.find(params[:id])
+  end
+  def edit
+    @animal = Animal.find(params[:id])
+  end
+  def update
+    @animal = Animal.find(params[:id])
+    if @animal.update(animal_params)
+      flash[:notice] = "Animal updated successfully!"
+      redirect_to  animals_path
+    else
+      render :edit
+    end
+  end
+  def destroy
+    @animal = Animal.find(params[:id])
+    @animal.destroy
+    redirect_to animals_path
+  end
+
+
+  private
+  def animal_params
+    params.require(:animal).permit(:name)
+  end
 end
